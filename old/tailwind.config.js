15da68ffa49e1f05c782e16ddc317e18a85a0a4f
Abilash <abilash.s@maersk.com>
2022-10-16 18:37:44 +0530

    Updated Changes on API and layout

---
+++ old/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        darkColor: "#12181B",
+      },
+    },
+  },
+  plugins: [],
+};
